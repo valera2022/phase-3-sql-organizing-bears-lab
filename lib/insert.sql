54ce9f680238488afc8dae2b4bd3bab565d12004
GeralmyUrena <geralmyuri11@gmail.com>
2023-06-06 22:01:45 -0400

    commit tree 65842b4b4f46dc6de5e3c8edd1fa79d38a6f8e51

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',1,'M','yellow','anger',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',2,'M','yellow','anger',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',3,'M','yellow','anger',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',40,'M','yellow','anger',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',56,'M','yellow','anger',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',45,'M','yellow','anger',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',6,'M','yellow','anger',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,9,'M','yellow','anger',1);
